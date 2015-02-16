06988a4c9427e87059952cb9872299cef7735d7c
John Vasko III <j3vasko@gmail.com>
2015-02-16 19:21:42 +0000

    no debug message!

--- include/list/v1/function/member.php
+++ include/list/v1/function/member.php
@@ -212,7 +212,8 @@ function get_cycle_last_start($channel_parent_id, $datetime) {
 function insert_renewal_next(& $cycle, & $renewal, $channel_parent_id, $user_id, $point_id, $datetime) {
 	global $config;
 	global $prefix;
-	echo '<pre>'; print_r($cycle); echo '</pre>'; $exit;
+	if ($config['debug'] == 1)
+		echo '<pre>'; print_r($cycle); echo '</pre>';
 	# alias
 	$ncycle = & $cycle['next'];
 	$ccycle = & $cycle['current'];
--- include/list/v1/process/edit_process.php
+++ include/list/v1/process/edit_process.php
@@ -692,6 +692,9 @@ switch($type) {
 	case 'profile':
 		# todo delete file and db entry and reinsert (will autofix bad data)
 
+		# todo get file data for uploaded file remember ( enctype="multipart/form-data" )
+		# echo '<pre>'; print_r($_FILES); echo '</pre>'; exit;
+
 		# assuming the opposite of filer_write() - if db entry exist file already exists
 		if (!(get_db_single_value('
 				id
@@ -707,7 +710,7 @@ switch($type) {
 			$s1 = '/www/site/list/public/phpqrcode/1.1.4/temp/' . $_SESSION['login']['login_user_name'] . '.png';
 			include('phpqrcode/1.1.4/qrlib.php');
 
-			# todo make real public key functionality instad of the user_id hack
+			# todo make real public key functionality instead of the user_id hack
 			QRcode::png('https://' . $_SERVER['HTTP_HOST'] . '/host_portal/?public_key=' . (int)$_SESSION['login']['login_user_id'], $s1, 'L', 4, 2);
 
 			switch(filer_move($s1, 'list/v1/qrcode/')) {
