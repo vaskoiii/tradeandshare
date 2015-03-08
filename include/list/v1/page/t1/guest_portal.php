9b98b93700b4e70a610e508dedb0dd2d7281c382
John Vasko III <j3vasko@gmail.com>
2015-03-08 13:09:47 +0000

    multi printable ids

--- include/list/v1/page/t1/guest_portal.php
+++ include/list/v1/page/t1/guest_portal.php
@@ -26,13 +26,6 @@ along with Trade and Share.  If not, see <http://www.gnu.org/licenses/>.
 # todo move to when profile is updated
 # unless we implement with the md5 file system will not be scalable
 
-# easiest for testing here.
-# todo Move to process for profile_edit when complete
-
-# todo if new pubkey
-
-include('list/v1/inline/t1/header_after.php');
-
 ?>
 
 <div class="content_box"><?
@@ -40,127 +33,137 @@ if ($data['guest_portal']['user_id']['id']) {
 # supposed to be printed out
 # todo use contact link instead if possible ?> 
 	<style>
-		#tsid {
+		.content_box {
+			text-align: center;
+			}
+		.tsid {
 			background: #fff;
 			border: 1px dashed #000;
-			width: 308px;
-			margin: 0px -18px;
+			width: 306px;
+			/* margin: 0px -18px; */
 			text-align: left;
-			
+			display: inline-block;
 			}
-			#tsid_top {
+			.tsid_top {
 				margin: 5px;
 				}
-				#tsid_icon {
-					}
-					#tsid_icon img {
+				.tsid_icon {
+				}
+					.tsid_icon img {
 						vertical-align: middle;
 						width: 24px;
 						height: 24px;
+						margin-right: 5px;
 					}
-				#tsid_url {
+				.tsid_url {
 					padding-bottom: 2px;
 					text-align: left;
 					}
-			#tsid_body {
+			.tsid_body {
 				margin: 5px;
 				padding-bottom: 5px;
+				width: 296px;
 				}
-				#tsid_face {
+				.tsid_face {
+					text-align: center;
 					width: 128px;
 					height: 128px;
-					text-align: center;
 					}
-					#tsid_face img {
+					.tsid_face img {
 						vertical-align: middle;
-						max-width: 128px;
-						max-height: 128px;
+						max-width: 112px;
+						max-height: 112px;
 						}
-				#tsid_question {
+				.tsid_question {
 					width: 20px;
 					text-align: center;
 					font-size: 28px;
-					line-height: 28px;
+					line-height: 24px;
 					font-weight: bold;
 					}
-				#tsid_qrcode {
+				.tsid_qrcode {
 					width: 128px;
 					height: 128px;
 					text-align: center;
 					}
-					#tsid_qrcode img {
+					.tsid_qrcode img {
 						vertical-align: middle;
 						max-width: 128px;
 						max-height: 128px;
 						}
+		@media screen and (max-width: 480px) {
+			.content_box {
+				padding: 0px;
+			}
+		}
 	</style>
-	<center>
-	<div id="tsid">
-		<table id="tsid_top">
-			<tr>
-				<td id="tsid_icon">
-					<img src="/list/v1/theme/select_none/ts_icon.png" />
-				</td>
-				<td id="tsid_url">
-					<a href="/">https://<?= to_html($_SERVER['HTTP_HOST']); ?></a>
-				</td>
-			</tr>
-		</table>
-		<table id="tsid_body">
-			<tr>
-				<td id="tsid_face"><?
-					$s1 = '/list/v1/theme/select_none/ts_icon_256x256.png';
-					if (!empty($data['guest_portal']['face_filer_id']))
-						$s1 = '/file/?id=' . (int)$data['guest_portal']['face_filer_id']; ?> 
-					<a href="/"><img src="<?= $s1; ?>" /></a>
-				</td>
-				<td id="tsid_question">
-					?
-					<br />
-					=
-				</td>
-				<td id="tsid_qrcode">
-					<a href="/host_portal/?public_key=<?= get_db_single_value('
-								value
-							from
-								' . $config['mysql']['prefix'] . 'pubkey
-							where
-								user_id = ' . (int)$_SESSION['login']['login_user_id']
-						, 0); ?>"
-					><img src="/file/?id=<?= (int)$data['guest_portal']['user_id']['id']; ?>" /></a>
-				</td>
-			</tr>
-		</table>
-	</div>
-	</center>
-	
-	<? if (0) { ?>
-	<center>
-	<div style="background: #fff; width: 286px; margin: 0px -16px; padding: 0px; border: 1px dashed #000;">
-		<table style="margin: 8px 0px; padding: 0px;">
-			<tr>
-				<td>
-					<img src="/list/v1/theme/select_none/ts_icon.png" style="width: 24px; height: 24px;" />
-				</td>
-				<td style="vertical-align: top; padding-top: 2px;">
-					<a href="/">https://<?= to_html($_SERVER['HTTP_HOST']); ?></a>
-				</td>
-			</tr>
-		</table>
-		<div>
-			<a href="/"><img src="/list/v1/theme/select_none/ts_icon_256x256.png" style="width: 256px; height: 256px; border: 1px solid black;" /></a>
-		</div>
-		<div style="font-size: 48px; font-weight: bold; margin-bottom: 5px;">
-			? =
-		</div>
-		<div style="margin-bottom: 10px;">
-			<img src="/phpqrcode/1.1.4/temp/list.png" style="width: 256px; height: 256px; border: 1px solid black;" /></a>
-		</div>
-	</div>
-	</center>
-	<? } ?> 
-	<? # the foollowing may be useful but may also be overly complicated ?>
-	<!--
+<?
+function guest_portal_print_id($minnify = 2) {
+	global $data;
+	global $config;
+	ob_start(); ?> 
+		<div class="tsid">
+			<table class="tsid_top">
+				<tr>
+					<td class="tsid_icon">
+						<img src="/list/v1/theme/select_none/ts_icon.png" />
+					</td>
+					<td class="tsid_url">
+						<a href="/">https://<?= to_html($_SERVER['HTTP_HOST']); ?></a>
+					</td>
+				</tr>
+			</table>
+			<table class="tsid_body">
+				<tr>
+					<td class="tsid_face"><?
+						$s1 = '/list/v1/theme/select_none/ts_icon_256x256.png';
+						if (!empty($data['guest_portal']['face_filer_id']))
+							$s1 = '/file/?id=' . (int)$data['guest_portal']['face_filer_id']; ?> 
+						<a href="/"><img src="<?= $s1; ?>" /></a>
+					</td>
+					<td class="tsid_question">
+						?
+						<br />
+						=
+					</td>
+					<td class="tsid_qrcode">
+						<a href="/host_portal/?public_key=<?= get_db_single_value('
+									value
+								from
+									' . $config['mysql']['prefix'] . 'pubkey
+								where
+									user_id = ' . (int)$_SESSION['login']['login_user_id']
+							, 0); ?>"
+						><img src="/file/?id=<?= (int)$data['guest_portal']['user_id']['id']; ?>" /></a>
+					</td>
+				</tr>
+			</table>
+		</div><?
+	$html = ob_get_contents();
+	ob_clean();
+
+	if ($minnify == 1) {
+	    $search = array(
+		'/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+		'/[^\S ]+\</s',  // strip whitespaces before tags, except space
+		'/(\s)+/s'       // shorten multiple whitespace sequences
+	    );
+	    $replace = array(
+		'>',
+		'<',
+		'\\1'
+	    );
+	    $html = preg_replace($search, $replace, $html);
+	}
+	return $html;
+} ?>
+
+<div id="guest_portal_id_container">
+	<?= guest_portal_print_id(); ?> 
+</div><?
+
+# the following may be useful but may also be overly complicated
+if (0) { ?> 
 	<h3>Enter</h3>
 	<dl>
 		<dt>Lock</dt>
@@ -175,9 +178,8 @@ if ($data['guest_portal']['user_id']['id']) {
 			<dd>Unset your locks</dd>
 		<dt>View</dt>
 			<dd>See your teams</dd>
-	</dl>
-	-->
-<?
+	</dl><? 
+}
 }
 else { ?>
 <center>
@@ -186,7 +188,18 @@ else { ?>
 } ?> 
 </div>
 
+<script>
+	function guest_portal_print_more() {
+		o1 = document.createElement('span');
+		o1.innerHTML = '<?= guest_portal_print_id(1); ?>';
+		did('guest_portal_id_container').appendChild(o1);
+	}
+</script>
+
 <div class="menu_1">
+	<ul style="margin: 0px auto; text-align: center;">
+		<li><a href="javascript: guest_portal_print_more();">Print More</a></li>
+	</ul>
 </div>
 
 <div class="menu_2">
